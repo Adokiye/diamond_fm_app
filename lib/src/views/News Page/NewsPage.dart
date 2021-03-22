ac4bafec187e44456217dc910ae20621143a48d7
Abdulwahab Abdulrasaq <58413440+Asquare17@users.noreply.github.com>
2021-03-22 12:40:53 +0100

    audio service added

--- lib/src/views/News Page/NewsPage.dart
+++ lib/src/views/News Page/NewsPage.dart
@@ -3,6 +3,7 @@ import 'package:diamond_fm_app/models/article_model.dart';
 import 'package:diamond_fm_app/src/components/LoadingCircle.dart';
 import 'package:diamond_fm_app/src/components/MyScaffold.dart';
 import 'package:diamond_fm_app/src/components/NewsCard.dart';
+import 'package:diamond_fm_app/src/views/home/ListenPage.dart';
 import 'package:flutter/material.dart';
 
 class NewsPage extends StatefulWidget {
@@ -11,7 +12,7 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
-  List<ArticleModel> articles = new List<ArticleModel>();
+  List<ArticleModel> articles = [];
   bool _loading = true;
   @override
   void initState() {
@@ -34,34 +35,42 @@ class _NewsPageState extends State<NewsPage> {
   Widget build(BuildContext context) {
     getNews();
 
-    return MyScaffold(
-      title: 'News',
-      body: _loading
-          ? loadingCircle()
-          : SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children:[
-                    Text('News Publisher Website: https://diamondfm.net/news',
-                    textAlign:TextAlign.center,
-      style: TextStyle(color: Colors.white,fontSize:12),
-                    ),
-                      Text('News Publisher Contact Email: diamondfm88.7@gmail.com',
-                    textAlign:TextAlign.center,
-
-      style: TextStyle(color: Colors.white,fontSize:12),
-                    ),
-                    ListView.builder(
-                    physics: ClampingScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: articles.length,
-                    itemBuilder: (context, index) {
-                      return NewsCard(article: articles[index]);
-                    }),])
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (BuildContext context) => ListenPage()));
+        return false;
+      },
+      child: MyScaffold(
+        title: 'News',
+        body: _loading
+            ? loadingCircle()
+            : SingleChildScrollView(
+                child: Container(
+                    padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'News Publisher Website: https://diamondfm.net/news',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          ),
+                          Text(
+                            'News Publisher Contact Email: diamondfm88.7@gmail.com',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          ),
+                          ListView.builder(
+                              physics: ClampingScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: articles.length,
+                              itemBuilder: (context, index) {
+                                return NewsCard(article: articles[index]);
+                              }),
+                        ])),
               ),
-            ),
+      ),
     );
   }
 }
